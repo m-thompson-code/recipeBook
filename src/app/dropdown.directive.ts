da3dbd50ba3cf9fc239178ca49189024612bc268
moomoomamoo <myt5@nau.edu>
2017-01-11 14:44:02 -0800

    Changes how recipes are got to a Service

--- src/app/dropdown.directive.ts
+++ src/app/dropdown.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[rbDropdown]'
 })
 export class DropdownDirective {
-
-  constructor() { }
-
+  @HostBinding('class.open') get opened() {
+    //the class open will only be binded if true
+    return this.isOpen;
+    // Note that bootstrap has a lot of styling already prepared
+    // its css handles the dropdown effect (we are just making it happen in our own way)
+  }
+  //mouseover mouseleave
+  @HostListener('click') open() {
+    this.isOpen = true;
+  }
+  @HostListener('mouseover') open2() {
+    this.isOpen = true;
+  }
+  @HostListener('mouseleave') close() {
+    this.isOpen = false;
+  }
+  private isOpen = false;
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'View tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
-    new Recipe('Summer Salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
-  ];
+  recipes: Recipe[] = [];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
   recipe: Recipe = new Recipe("Dummy", "Dummy", "http://vignette1.wikia.nocookie.net/harvestmoondscute/images/a/a4/Cow.jpg/revision/latest?cb=20120526004550", []);
   
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe: Recipe) {
