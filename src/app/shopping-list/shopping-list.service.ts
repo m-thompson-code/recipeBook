deee4cb34e1469767e009e752ee3622bacb57470
moomoomamoo <myt5@nau.edu>
2017-01-11 15:26:53 -0800

    added shopping-list functionality (through shopping-list.service)

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,15 +4,18 @@ import { Ingredient } from '../shared/ingredient';
 
 //@Injectable()
 export class ShoppingListService {
-  private items: Ingredient[];
+  private items: Ingredient[] = [];
 
   constructor() { }
   
   getItems() {
     return this.items;
   }
+  
   addItems(items: Ingredient[]) {
     // Uses the default push (Array) for every element of items to this.items
     Array.prototype.push.apply(this.items, items);
+    //console.log(this.items);
+    //console.log(items);
   }
 }
